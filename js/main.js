1a60d14a63819a1499f9c7aa1744e630b80554e9
Ahmed <a.abdelaziz44@gmail.com>
2018-07-21 17:20:22 +0200

    Separating the data from the DOM

---
+++ js/main.js
@@ -0,0 +1,28 @@
+"use strict";
+
+// emojify returns the corresponding emoji image
+function emojify(name) {
+    var out = `<img src="emojis/` + name + `.png">`;
+    return out;
+}
+
+// cast returns a spell (function) that decorates the wizard
+function cast(emoji) {
+    if (!emoji) {
+        emoji = "¯\\_(ツ)_/¯";
+    }
+	return function (wizard) {
+		return emoji + wizard + emoji;
+	};
+}
+
+var app = new Vue({
+    el: "#app",
+    data: {
+        wizard: emojify("wizard")
+    },
+    methods: {
+        lumos: cast(emojify('lumos')),
+        incendio: cast(emojify("incendio"))
+    }
+});
