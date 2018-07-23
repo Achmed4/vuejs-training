6d5d43fd5977b8e21d7820869404154e199b7971
Ahmed <a.abdelaziz44@gmail.com>
2018-07-23 18:29:13 +0200

    Lights, Camera, Interaction

--- js/main.js
+++ js/main.js
@@ -6,43 +6,40 @@ function emojify(name) {
     return out;
 }
 
-// cast returns a spell (function) that decorates the wizard
-function cast(emoji) {
-    var magic = emojify("magic");
-	return function (wizard) {
-		return wizard + " " + magic + " " + emoji + " " + magic;
-	};
-}
-// vue components
-Vue.component("harry", {
-    template: `<p>${emojify("harry")}</p>`
-});
-Vue.component("ron", {
-    template: `<p>${emojify("ron")}</p>`
-});
-Vue.component("hermione", {
-    template: `<p>${emojify("hermione")}</p>`
-});
-
-// vue components with props (properties)
-Vue.component("wizard", {
-    props: ["name", "cast"],
-    template: `<p v-html="cast(name)"><slot></slot></p>` //<slot></slot> to uderstand input between opening and closing component tag
-});
-
 var app = new Vue({
     el: "#app",
     data: {
-		harry       : emojify("harry"      ),
-		ron         : emojify("ron"        ),
-		hermione    : emojify("hermione"   )
+		active: emojify("sirius--man"),
+		// sirius is an object that contains two states: man and dog
+		sirius: {
+			man: emojify("sirius--man"),
+			dog: emojify("sirius--dog")
+		}
 	},
     methods: {
-		// oculus_reparo returns a spell (function) that repairs glasses		
-		oculus_reparo: cast(emojify("oculus-reparo")),		
-		// wingardium_leviosa returns a spell (function) that levitates an object
-		wingardium_leviosa: cast(emojify("wingardium-leviosa")),
-		// alohomora returns a spell (function) that unlocks a door
-		alohomora: cast(emojify("alohomora"))
+		// an animagus is a wizard whom can shapeshift
+		animagus: function () {
+			this.active = (
+				this.active == this.sirius.man ?
+					this.sirius.dog :
+					this.sirius.man
+			)
+		},
+		// breathe returns the corresponding .breathe--*
+		breathe: function () {
+			return (
+				this.active == this.sirius.man ?
+					"breathe--man" :
+					"breathe--dog"
+			)
+		},
+		// background returns the corresponding background
+		background: function () {
+			return (
+				this.active == this.sirius.man ?
+					"" :
+					"black"
+			)
+		}
 	}
 });
